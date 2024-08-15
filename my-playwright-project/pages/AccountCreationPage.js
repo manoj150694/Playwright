26fcc3d4880649ff57eba9250fdc418c185a10b7
ranjithkr028 <ranjithkr028@gmail.com>
2024-08-15 17:19:53 +0100

    review comments update - xpath changes

--- my-playwright-project/pages/AccountCreationPage.js
+++ my-playwright-project/pages/AccountCreationPage.js
@@ -2,7 +2,7 @@ const { expect } = require("@playwright/test");
 exports.AccountCreationPage = class AccountCreationPage{
     constructor(page) {
         this.page = page;
-        this.msg_AccountCreation = page.locator(`xpath=//h2[@class='title text-center']/..//following-sibling::h2/b`);
+        this.msg_AccountCreation = page.locator('//h2[@class="title text-center"]/..//following-sibling::h2/b');
         this.btn_Continue = page.locator(`xpath=//a[contains(text(),'Continue')]`);
     }
 
--- my-playwright-project/pages/ProductDescriptionPage.js
+++ my-playwright-project/pages/ProductDescriptionPage.js
@@ -2,12 +2,12 @@ const { expect } = require("@playwright/test");
 exports.ProductDescriptionPage = class ProductDescriptionPage {
     constructor(page) {
         this.page = page;
-        this.plusBtn_Women = page.locator(`xpath=//div[@id="Women"]/..//preceding-sibling::a/span`)
-        this.plusBtn_Men = page.locator(`xpath=//div[@id="Men"]/..//preceding-sibling::a/span`)
-        this.plusBtn_Kids = page.locator(`xpath=//div[@id="Kids"]/..//preceding-sibling::a/span`)
-        this.category_MenTshirts = page.locator(`xpath=//div[@id="Men"]/..//following-sibling::li/a[contains(text(),'Tshirts')]`)
-        this.lbl_AddToCart = page.locator(`(xpath=//div[@class="productinfo text-center"]/..//following-sibling::a)[1]`)
-        this.overlay_AddToCart = page.locator(`xpath=(//div[@class="overlay-content"]/..//following-sibling::a)[1]`)
+        this.plusBtn_Women = page.locator('//div[@id="Women"]/..//preceding-sibling::a/span')
+        this.plusBtn_Men = page.locator('//div[@id="Men"]/..//preceding-sibling::a/span')
+        this.plusBtn_Kids = page.locator('//div[@id="Kids"]/..//preceding-sibling::a/span')
+        this.category_MenTshirts = page.locator('//div[@id="Men"]/..//following-sibling::li/a[contains(text(),"Tshirts")]')
+        this.lbl_AddToCart = page.locator('//div[@class="productinfo text-center"]/..//following-sibling::a)[1]')
+        this.overlay_AddToCart = page.locator('(//div[@class="overlay-content"]/..//following-sibling::a)[1]')
     }
 
     async selectCategoryMens() {
